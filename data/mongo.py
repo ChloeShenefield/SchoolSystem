72bff351b09dd103bde3470a7c8039e7321d783a
ettynan <ettynan@gmail.com>
2020-06-29 19:37:06 -0700

    initial commit to erin branch

--- data/mongo.py
+++ data/mongo.py
@@ -7,17 +7,24 @@ import sys
 import getpass
 import pymongo
 
-from project2.SchoolSystem.users.model import User, Admin, Teacher, Student
-from project2.SchoolSystem.data.logger import get_logger
+from SchoolSystem.users.model import User, Admin, Teacher, Student
+from SchoolSystem.data.logger import get_logger
 
 _log = get_logger(__name__)
 
 try:
-    _scl = pymongo.MongoClient(os.environ.get('MONGO_URI')).project2
+    _scl = pymongo.MongoClient(os.environ.get('MONGO_URI')).SchoolSystem
 except:
     _log.exception('Could not connect to Mongo')
     raise
 
+def login(username: str, password: str):
+    '''A function that takes in a username and returns a user object'''
+    _log.info('Attempting to retrieve user from database')
+    query_dict = {'username': username}
+    user_dict = _scl.users.find_one(query_dict)
+    _log.debug(type(user_dict))
+    return User.from_dict(user_dict) if user_dict else None
 
 def _get_id():
     '''Retrieves the next id in the database and increments it'''
@@ -26,7 +33,7 @@ def _get_id():
                                             return_document=pymongo.ReturnDocument.AFTER)['count']
 
 if __name__ == '__main__':
-    _log.info('Running Mongo script: dropping collections from project2 database')
+    _log.info('Running Mongo script: dropping collections from SchoolSystem database')
     _log.info(_bank.list_collection_names())
     _scl.users.drop()
 
